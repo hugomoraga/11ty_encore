706dc1415e2193fd7124448d3e46c72b7966c963
Hugo M <hg.moraga@gmail.com>
2022-02-14 19:23:08 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+  .setOutputPath('build/')
+  .setPublicPath('/11ty_encore/build')
+  .setManifestKeyPrefix('build')
+  .addEntry('app', './js/app.js')
+  .enableSingleRuntimeChunk()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableSassLoader()
+;
+
+if (Encore.isProduction()) {
+  Encore
+    .cleanupOutputBeforeBuild()
+    .enableVersioning()
+  ;
+}
+
+module.exports = Encore.getWebpackConfig();
